fafba86b29f32a88d4376c6bea6564b0f42c06cc
George C <1lclink2@att.net>
2025-11-06 12:22:35 -0500

    Initial Commit

---
+++ SideMenuTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SideMenuTutorial
+//
+//  Created by George Clinkscales on 11/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SideMenuTutorial/SideMenuTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuTutorialApp.swift
+//  SideMenuTutorial
+//
+//  Created by George Clinkscales on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
